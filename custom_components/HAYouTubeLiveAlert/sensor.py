15c553d799f8ae6c49f7febd5000966dec25545a
iKeLi <ikeli@hotaimotor.com.tw>
2024-08-15 17:43:52 +0800

    fix

--- custom_components/HAYouTubeLiveAlert/sensor.py
+++ custom_components/HAYouTubeLiveAlert/sensor.py
@@ -1,77 +1,44 @@
-import homeassistant.helpers.config_validation as cv
-import logging
 import datetime
+import logging
 import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers import config_validation as cv
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'hayoutubelivealert'
-SCAN_INTERVAL = datetime.timedelta(minutes=30)  # 將更新間隔設為30分鐘
+SCAN_INTERVAL = datetime.timedelta(hours=1)  # 將更新間隔設為1小時
 
 CONF_API_KEY = 'api_key'
 CONF_CHANNEL_ID = 'channel_id'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-	vol.Required(CONF_API_KEY): cv.string,
-	vol.Required(CONF_CHANNEL_ID): cv.string,
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Required(CONF_CHANNEL_ID): cv.string,
 })
 
-class YouTubeSensor(Entity):
-	def __init__(self, api_key, channel_id):
-		self._api_key = api_key
-		self._channel_id = channel_id
-		self._state = None
-		self._live_url = None
-
-	@property
-	def name(self):
-		return "YouTube Live Alert"
-
-	@property
-	def state(self):
-		return self._state
-
-	@property
-	def extra_state_attributes(self):
-		return {
-			"live_url": self._live_url
-		}
-
-	def update(self):
-		youtube = build('youtube', 'v3', developerKey=self._api_key)
-		request = youtube.search().list(part='snippet', channelId=self._channel_id, type='video', eventType='live')
-		response = request.execute()
-
-		if response['items']:
-			self._state = "Live"
-			self._live_url = f"https://www.youtube.com/watch?v={response['items'][0]['id']['videoId']}"
-		else:
-			self._state = "Offline"
-			self._live_url = None
-
 def setup_platform(hass, config, add_entities, discovery_info=None):
-	api_key = config.get(CONF_API_KEY)
-	channel_id = config.get(CONF_CHANNEL_ID)
+    api_key = config[CONF_API_KEY]
+    channel_id = config[CONF_CHANNEL_ID]
 
-	if not api_key or not channel_id:
-		_LOGGER.error("API key or Channel ID not provided")
-		return False
+    if not api_key or not channel_id:
+        _LOGGER.error("You must provide an API key and channel ID")
+        return False
 
-	add_entities([YouTubeSensor(api_key, channel_id)], True)
+    add_entities([YouTubeSensor(api_key, channel_id)], True)
 
 class YouTubeSensor(Entity):
     def __init__(self, api_key, channel_id):
+        self._state = None
         self._api_key = api_key
         self._channel_id = channel_id
-        self._state = None
+        self._name = "YouTube Live Stream"
         self._live_url = None
 
     @property
     def name(self):
-        return "YouTube Live Alert"
+        return self._name
 
     @property
     def state(self):
@@ -89,17 +56,22 @@ class YouTubeSensor(Entity):
             request = youtube.search().list(part='snippet', channelId=self._channel_id, type='video', eventType='live')
             response = request.execute()
 
-            _LOGGER.debug("YouTube API response: %s", response)  # 增加日誌以檢查 API 回應
-
             if response['items']:
                 self._state = "Live"
                 self._live_url = f"https://www.youtube.com/watch?v={response['items'][0]['id']['videoId']}"
-                _LOGGER.info("Live stream found: %s", self._live_url)
             else:
-                self._state = "Not Live"
+                self._state = "Offline"
                 self._live_url = None
-                _LOGGER.info("No live stream found.")
+
+        except HttpError as e:
+            if e.resp.status == 403 and 'quotaExceeded' in e.content.decode():
+                _LOGGER.error("YouTube API quota exceeded.")
+            else:
+                _LOGGER.error(f"HTTP error occurred: {e}")
+            self._state = "Error"
+            self._live_url = None
+
         except Exception as e:
-            _LOGGER.error("Error fetching data from YouTube API: %s", e)
+            _LOGGER.error(f"An error occurred: {e}")
             self._state = "Error"
             self._live_url = None
